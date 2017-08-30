5431f453b6b1432333310b2d801e426135cb54f3
bocui <cuishaobo16@outlook.com>
2017-08-30 17:11:14 +0800

    add merge sort

---
+++ MergeSort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void Merge(const int *SR, int *TR, int low, int high, int mid);
+
+void MergeSort(const int *SR, int *TR, int low, int high) 
+{
+    int *TMP = new int[sizeof(SR) / sizeof(*SR)]();
+    if (low == high) TR[low] = SR[low];
+    else {
+	int mid = (low + high) / 2;
+	MergeSort(SR, TMP, low, mid);
+	MergeSort(SR, TMP, mid + 1, high);
+	Merge(TMP, TR, low, mid, high);
+    } 
+}
