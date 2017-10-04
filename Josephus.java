e3fbb3e6481df95affc505ca77a3dcad55fe0a32
bocui <cuishaobo16@outlook.com>
2017-10-04 19:18:46 +0800

    Josephus

---
+++ Josephus.java
@@ -0,0 +1,33 @@
+// It is the solution for Josephus problem(Java implementation)
+
+public class Node {
+    public int value;
+    public Node next;
+    public Node(int data) {
+	this.value = data;
+    }
+}
+
+public Node josephusKill(Node head, int m) {
+    if (head == null || head.next == head || m < 1) {
+	return head;
+    }
+
+    // Node last is to denote the final Node. 
+    Node last = head;
+    while (last.next != head) {
+	last = last.next;
+    }
+
+    int count = 0;
+    while (head != last) {
+	if (++count == m) {
+	    last.next = head.next;
+	    count = 0;
+	} else {
+	    last = last.next;
+	}
+	head = last.next;
+    }
+    return head;
+}
