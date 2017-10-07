accce79e52cc9d1de8d401701f72078624367e17
bocui <cuishaobo16@outlook.com>
2017-10-07 23:18:14 +0800

    fin

--- Dynamic_Programming/fibonacci/typical_fibon/fibo_ON.java
+++ Dynamic_Programming/fibonacci/typical_fibon/fibo_ON.java
@@ -1,1 +1,18 @@
-public int
+public int fibON(int n) {
+    if (n < 1) {
+	return 0;
+    }
+    if (n == 1 || n == 2) {
+	return 1;
+    }
+    
+    int res = 0;
+    int pre = 1;
+    int prepre = 1;
+    for (int i = 3; i < n; i++) {
+        pre = res;
+	res = res + pre;
+        res = prepre + pre;
+	pre = res;
+    }
+}
