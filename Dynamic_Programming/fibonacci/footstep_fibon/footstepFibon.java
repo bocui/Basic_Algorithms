0f51da4c57ab04388371585084e5e5c973cfad59
bocui <cuishaobo16@outlook.com>
2017-10-09 08:33:23 +0800

    add footstepFibon algorithm in O(N)

--- Dynamic_Programming/fibonacci/footstep_fibon/footstepFibon.java
+++ Dynamic_Programming/fibonacci/footstep_fibon/footstepFibon.java
@@ -1,5 +1,5 @@
 // Algorithm in O(2^N) 
-public int step(int n) {
+public int stepO2N(int n) {
     if (n < 1) {
 	return 0;
     }
@@ -9,4 +9,22 @@ public int step(int n) {
     return step(n - 1) + step(n - 2);
 }
 
-//
+//Algorithm in O(N)
+
+public int stepON(int n) {
+    if (n < 1) {
+	return 0;
+    }
+    if (n == 1 || n == 2) {
+	return n;
+    }
+    int res = 2;
+    int pre = 1;
+    int tmp = 0;
+    for (int i = 3; i <= n; i++) {
+	tmp = res;
+	res = res + pre;
+	pre = tmp;
+    } 
+    return res;
+}
