8df6ac2e8feb22aab281ca7474ec1819ca38bcfc
bocui <cuishaobo16@outlook.com>
2017-10-11 21:13:48 +0800

    rm Dijkstra.cpp file

--- Dijkstra.cpp
+++
@@ -1,6 +0,0 @@
-// === This file is for the Dijkstra algorithm -- CPP implementation ===
-// Reference Link: http://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/
-
-#include <stdio.h>
-#include <limits.h>
-void dijkstra(int graph[][], int src)
